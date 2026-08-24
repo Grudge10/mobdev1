aef0529ace70a8fa69c7e910079f3b36691033a9
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-08-24 14:07:42 +0000

    finished act 4

--- main.dart
+++ main.dart
@@ -1,29 +1,53 @@
-import 'dart:io';
-
 void main(List<String> arguments) {
-  print("Enter your name:");
-  String name = stdin.readLineSync()!;
+  // String Concatenation
+  String firstName = "John";
+  String lastName = "Doe";
+  String fullName = firstName + " " + lastName;
+  print('Full Name: $fullName');
 
-  print("Enter your age:");
-  int age = int.parse(stdin.readLineSync()!);
+  // String Interpolation
+  print('Full Name: $firstName $lastName');
 
-  print("Enter your height(cm)");
-  double height = double.parse(stdin.readLineSync()!);
+  // String Length
+  String language = "Dart";
+  int languageLength = language.length;
+  print('String Length: $languageLength');
 
-  print("Enter your weight(kg):");
-  num weight = num.parse(stdin.readLineSync()!);
+  // Capitalization
+  String text = "hello, world!";
+  String textUppercase = text.toUpperCase();
+  print('Capitalization: $textUppercase');
 
-  print("Enter your favorite color:");
-  dynamic color = stdin.readLineSync();
+  // Uncapitalization
+  String textLowercase = textUppercase.toLowerCase();
+  print('Uncapitalization: $textLowercase');
 
-  print("Do you have a dog?(true or false)");
-  bool haveADog = bool.parse(stdin.readLineSync()!);
+  // Trim
+  String spacedText = "     Hello <-space-> World      ";
+  String trimmedText = spacedText.trim();
+  print('Trim: $trimmedText');
 
-  print("\n----- About You -----");
-  print("Name: $name");
-  print("Age: $age");
-  print("Height: $height cm");
-  print("Weight: $weight kg");
-  print("Favorite color: $color");
-  print("Has a dog: $haveADog");
+  // Search
+  String brainrot = "skibidi";
+  bool searchedAnywhere = brainrot.contains('ibi');
+  bool searchedBeginning = brainrot.startsWith('ski');
+  bool searchedEnd = brainrot.endsWith('idi');
+  print('Searched Anywhere: $searchedAnywhere');
+  print('Searched Beginning: $searchedBeginning');
+  print('Searched End: $searchedEnd');
+
+  // (Search) and Replace
+  String randomText = "I use Java";
+  String replacedText = randomText.replaceAll("Java", "Dart");
+  print('Search and Replace: $replacedText');
+
+  // String extraction
+  String text2 = "Hello World";
+  String extractedText = text2.substring(0, 5);
+  print('String Extraction: $extractedText');
+
+  // String split
+  String fruits = "Apple,Banana,Orange";
+  List<String> result = fruits.split(",");
+  print('String Split: $result');
 }
